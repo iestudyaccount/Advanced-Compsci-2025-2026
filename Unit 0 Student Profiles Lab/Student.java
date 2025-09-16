6f7e93765390e235f9e1573db1a84709ce6ff6b7
iestudyaccount <ierlic1@hwemail.com>
2025-09-16 14:59:05 -0700

    Class Work 9/16/2025

---
+++ Unit 0 Student Profiles Lab/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+
+    private String name;
+    private int grade;
+    private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public String toString() {
+
+    }
+}
