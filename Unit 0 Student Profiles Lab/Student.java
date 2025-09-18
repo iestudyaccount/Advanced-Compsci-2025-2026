1c5c76345a78f91f8f316550bfb2e6ec7edd50ef
iestudyaccount <ierlic1@hwemail.com>
2025-09-18 11:49:40 -0700

    Class 9/18/2025

--- Unit 0 Student Profiles Lab/Student.java
+++ Unit 0 Student Profiles Lab/Student.java
@@ -3,6 +3,25 @@ public class Student {
     private String name;
     private int grade;
     private String id;
+    private String latestOpp;
+    private String recentFriend;
+
+    public String generateId() {
+        id = "" + (int)(Math.random() * 8 + 1) + (int)(Math.random() * 8 + 1) + (int)(Math.random() * 8 + 1) + "-" + (int)(Math.random() * 10) + (int)(Math.random() * 10) + (int)(Math.random() * 10) + (int)(Math.random() * 10);
+        return id;
+    }
+
+    public Student(String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+        this.id = generateId();
+    }
+
+    public Student(String name) {
+        this.name = name;
+        this.grade = 10;
+        this.id = generateId();
+    }
 
     public String getName() {
         return name;
@@ -16,7 +35,40 @@ public class Student {
         return id;
     }
 
-    public String toString() {
-
+    public void setName(String newName) {
+        name = newName;
     }
+    public void setGrade(int newGrade) {
+        grade = newGrade;
+    }
+
+    public void setId(String newId) {
+        id = newId;
+    }
+
+    public String toString() {
+        return name + " is a " + grade + "th grade student. Their ID is " + id + ".";
+    }
+    
+    public boolean equals(Student otherStudent) {
+        if (this.name.equals(otherStudent.name)
+        && this.grade == otherStudent.grade
+        && this.id.equals(otherStudent.id)
+        && this.latestOpp.equals(otherStudent.latestOpp)
+        ){
+            return true;
+        }
+        return false;
+    }
+    
+    public String vibeCheck(Student other) {
+        if(this.latestOpp == other.latestOpp){
+            this.recentFriend = other.recentFriend;
+            other.recentFriend = this.recentFriend;
+
+            return (this.recentFriend + " and " + other.recentFriend + " are now friends! They both dislike " + latestOpp + ".");
+        }
+        return (this.recentFriend + " and " + other.recentFriend + " are not friends.");
+    }
+
 }
---
+++ Unit 0 Student Profiles Lab/StudentTester.java
@@ -0,0 +1,15 @@
+public class StudentTester {
+    public static void main(String[] args) {
+        Student chris = new Student("Chris", 11);
+        chris.toString();
+
+        Student connie = new Student("Connie");
+        connie.toString();
+
+        Student lucasYu = new Student("Lucas", 11);
+        lucasYu.toString();
+
+        Student dozer = new Student("Dozer");
+        dozer.toString();
+    }
+}
