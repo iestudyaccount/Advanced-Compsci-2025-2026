8ba414a495751119559682e09eff52a66fddb4a4
iestudyaccount <ierlic1@hwemail.com>
2025-09-19 09:22:00 -0700

    Homework 9/19/2025

--- Unit 0 Student Profiles Lab/Student.java
+++ Unit 0 Student Profiles Lab/Student.java
@@ -6,23 +6,54 @@ public class Student {
     private String latestOpp;
     private String recentFriend;
 
+    // generates id for every new student
     public String generateId() {
-        id = "" + (int)(Math.random() * 8 + 1) + (int)(Math.random() * 8 + 1) + (int)(Math.random() * 8 + 1) + "-" + (int)(Math.random() * 10) + (int)(Math.random() * 10) + (int)(Math.random() * 10) + (int)(Math.random() * 10);
+        id = "" + (int) (Math.random() * 8 + 1)
+                + (int) (Math.random() * 8 + 1)
+                + (int) (Math.random() * 8 + 1)
+                + "-"
+                + (int) (Math.random() * 10)
+                + (int) (Math.random() * 10)
+                + (int) (Math.random() * 10)
+                + (int) (Math.random() * 10);
         return id;
     }
 
+    // constructor that contains student name and grade
     public Student(String name, int grade) {
         this.name = name;
         this.grade = grade;
         this.id = generateId();
+        this.latestOpp = "Jim";
+        this.recentFriend = "Tim";
     }
 
+    // constructor that only contains student name, defaults to 10th grade
     public Student(String name) {
         this.name = name;
         this.grade = 10;
         this.id = generateId();
+        this.latestOpp = "Jim";
+        this.recentFriend = "Tim";
     }
 
+    public Student(String name, int grade, String latestOpp, String recentFriend) {
+        this.name = name;
+        this.grade = grade;
+        this.id = generateId();
+        this.latestOpp = latestOpp;
+        this.recentFriend = recentFriend;
+    }
+
+    public Student(String name, String latestOpp, String recentFriend) {
+        this.name = name;
+        this.grade = 10;
+        this.id = generateId();
+        this.latestOpp = latestOpp;
+        this.recentFriend = recentFriend;
+    }
+
+    // getters
     public String getName() {
         return name;
     }
@@ -31,13 +62,15 @@ public class Student {
         return grade;
     }
 
-    public String getID() {
+    public String getId() {
         return id;
     }
 
+    // setters
     public void setName(String newName) {
         name = newName;
     }
+
     public void setGrade(int newGrade) {
         grade = newGrade;
     }
@@ -46,27 +79,28 @@ public class Student {
         id = newId;
     }
 
+    // methods
     public String toString() {
         return name + " is a " + grade + "th grade student. Their ID is " + id + ".";
     }
-    
+
     public boolean equals(Student otherStudent) {
         if (this.name.equals(otherStudent.name)
-        && this.grade == otherStudent.grade
-        && this.id.equals(otherStudent.id)
-        && this.latestOpp.equals(otherStudent.latestOpp)
-        ){
+                && this.grade == otherStudent.grade
+                && this.id.equals(otherStudent.id)
+                && this.latestOpp.equals(otherStudent.latestOpp)) {
             return true;
         }
         return false;
     }
-    
+
     public String vibeCheck(Student other) {
-        if(this.latestOpp == other.latestOpp){
+        if (this.latestOpp == other.latestOpp) {
             this.recentFriend = other.recentFriend;
             other.recentFriend = this.recentFriend;
-
-            return (this.recentFriend + " and " + other.recentFriend + " are now friends! They both dislike " + latestOpp + ".");
+            return (this.recentFriend + " and " + other.recentFriend
+                    + " are now friends! They both dislike "
+                    + latestOpp + ".");
         }
         return (this.recentFriend + " and " + other.recentFriend + " are not friends.");
     }
--- Unit 0 Student Profiles Lab/StudentTester.java
+++ Unit 0 Student Profiles Lab/StudentTester.java
@@ -2,14 +2,26 @@ public class StudentTester {
     public static void main(String[] args) {
         Student chris = new Student("Chris", 11);
         chris.toString();
+        chris.setGrade(12);
+        chris.toString();
+        chris.vibeCheck(chris);
 
         Student connie = new Student("Connie");
         connie.toString();
+        connie.setGrade(11);
+        connie.toString();
+        connie.vibeCheck(chris);
 
         Student lucasYu = new Student("Lucas", 11);
         lucasYu.toString();
+        lucasYu.setGrade(10);
+        lucasYu.toString();
+        lucasYu.vibeCheck(connie);
 
         Student dozer = new Student("Dozer");
         dozer.toString();
+        dozer.setGrade(12);
+        dozer.toString();
+        dozer.vibeCheck(lucasYu);
     }
 }
